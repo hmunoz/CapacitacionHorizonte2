b9aad86973ba6aa1ae40b6db9174e23467084e61
hmunozMac <hmunoz@unrn.edu.ar>
2016-08-30 17:23:35 -0300

    Recorrido con la VM

--- app/src/main/java/ar/edu/unrn/lia/capacitacionhorizonte2/gps/MainActivity.java
+++ app/src/main/java/ar/edu/unrn/lia/capacitacionhorizonte2/gps/MainActivity.java
@@ -20,6 +20,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
@@ -28,8 +29,11 @@ import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 
-import ar.edu.unrn.lia.capacitacionhorizonte2.maps.MapsActivity;
+import java.text.DateFormat;
+import java.util.Date;
+
 import ar.edu.unrn.lia.capacitacionhorizonte2.R;
+import ar.edu.unrn.lia.capacitacionhorizonte2.maps.MapsActivity;
 import ar.edu.unrn.lia.capacitacionhorizonte2.preference.SettingsActivity;
 import ar.edu.unrn.lia.capacitacionhorizonte2.service.MyService;
 import butterknife.BindView;
@@ -41,11 +45,11 @@ public class MainActivity extends AppCompatActivity implements
         LocationListener {
 
 
-    private static final String TAG = MainActivity.class.getCanonicalName();
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     static final int REQUEST_LOCATION = 2;
     static final int REQUEST_IMAGE_CAPTURE = 1;
-
+    private String mLastUpdateTime;
     @BindView(R.id.toolbar)
     Toolbar toolbar;
     @BindView(R.id.content_main)
@@ -54,11 +58,14 @@ public class MainActivity extends AppCompatActivity implements
     FloatingActionButton fab;
     @BindView(R.id.imgPreview)
     ImageView imageView;
+    @BindView(R.id.latitude_textview)
+    TextView latitudeTextview;
+    @BindView(R.id.longitude_textview)
+    TextView longitudeTextview;
 
 
     private GoogleApiClient mGoogleApiClient = null;
     private LocationRequest mLocationRequest = null;
-    private Location mLocation = null;
 
 
     @Override
@@ -70,12 +77,6 @@ public class MainActivity extends AppCompatActivity implements
         setSupportActionBar(toolbar);
 
         setupGoogleAPIClient();
-        createLocationRequest();
-
-        if (mGoogleApiClient.isConnected()) {
-            startLocationUpdate();
-        }
-
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -91,35 +92,30 @@ public class MainActivity extends AppCompatActivity implements
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             startActivity(new Intent(this, SettingsActivity.class));
             return true;
-        }else  if (id == R.id.action_map) {
+        } else if (id == R.id.action_map) {
             startActivity(new Intent(this, MapsActivity.class));
             return true;
-        }else if (id ==R.id.action_start_stope){
+        } else if (id == R.id.action_start_stope) {
             //Iniciar/Stop Servicios
-            Intent intent = new Intent(this,MyService.class);
-            if (MyService.isInstanceCreated()){
+            Intent intent = new Intent(this, MyService.class);
+            if (MyService.isInstanceCreated()) {
                 stopService(intent);
-            }else{
+            } else {
                 startService(intent);
             }
 
-            return  true;
+            return true;
         }
 
         return super.onOptionsItemSelected(item);
@@ -128,9 +124,9 @@ public class MainActivity extends AppCompatActivity implements
 
     @Override
     protected void onStart() {
+        super.onStart();
         // Connect the client.
         mGoogleApiClient.connect();
-        super.onStart();
 
 
     }
@@ -138,15 +134,16 @@ public class MainActivity extends AppCompatActivity implements
     @Override
     protected void onStop() {
         // Disconnecting the client invalidates it.
-        mGoogleApiClient.disconnect();
         super.onStop();
+        if (mGoogleApiClient.isConnected()) {
+            mGoogleApiClient.disconnect();
+        }
     }
 
 
     @Override
     protected void onPause() {
         super.onPause();
-        stopLocationUpdate();
     }
 
     @Override
@@ -157,44 +154,38 @@ public class MainActivity extends AppCompatActivity implements
 
 
     private void setupGoogleAPIClient() {
-        if (mGoogleApiClient == null) {
             mGoogleApiClient = new GoogleApiClient.Builder(this)
                     .addApi(LocationServices.API)
                     .addConnectionCallbacks(this)
                     .addOnConnectionFailedListener(this)
                     .build();
-        }
+    }
+
+    private void createLocationRequest() {
+        mLocationRequest = LocationRequest.create();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setInterval(1000);// Update location every second
+        mLocationRequest.setFastestInterval(5000);
+        mLocationRequest.setSmallestDisplacement(20); //Set the minimum displacement between location updates in meters
     }
 
 
     @Override
     public void onConnected(@Nullable Bundle bundle) {
 
+        //Android 7.0 permisos
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this,
                     new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, REQUEST_LOCATION);
-        } else {
-            mLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
-            if (mLocation != null) {
-                // LatLng latLng = new LatLng(mLocationRequest.getLatitude(),mLocationRequest.getLongitude());
-                // mMap.addMarker(new MarkerOptions().position(latLng).title("Programmatically Current Loaction"));
-                // mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
-                Toast.makeText(this, "getLatitude() = " + String.valueOf(mLocation.getLatitude()) + "\n getLongitude() = " + String.valueOf(mLocation.getLongitude()), Toast.LENGTH_LONG).show();
-            }
-
-            startLocationUpdate();
-
         }
 
+        createLocationRequest();
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+
     }
 
 
-    private void createLocationRequest() {
-        mLocationRequest = LocationRequest.create();
-        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-        mLocationRequest.setInterval(1000); // Update location every second
-        mLocationRequest.setSmallestDisplacement(20);
-    }
+
 
     private void startLocationUpdate() {
 
@@ -202,6 +193,7 @@ public class MainActivity extends AppCompatActivity implements
             ActivityCompat.requestPermissions(this,
                     new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, REQUEST_LOCATION);
         } else {
+            //this para la interface LocationListener
             LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
         }
     }
@@ -210,10 +202,11 @@ public class MainActivity extends AppCompatActivity implements
         LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
     }
 
+
     @Override
     public void onConnectionSuspended(int i) {
+        Log.i(TAG, "Connection Suspended");
         mGoogleApiClient.connect();
-        Log.i(TAG, "GoogleApiClient connection has been suspend");
     }
 
     @Override
@@ -224,8 +217,11 @@ public class MainActivity extends AppCompatActivity implements
 
     @Override
     public void onLocationChanged(Location location) {
-        this.mLocation = location;
-        Toast.makeText(this, "onLocationChanged: " + mLocation.toString(), Toast.LENGTH_LONG).show();
+        mLastUpdateTime = DateFormat.getTimeInstance().format(new Date());
+        latitudeTextview.setText(String.valueOf(location.getLatitude()));
+        longitudeTextview.setText(String.valueOf(location.getLongitude()));
+        Snackbar.make(container, "Updated: " + mLastUpdateTime, Snackbar.LENGTH_SHORT).show();
+
     }
 
 
